3d58ee145dbb2f6a624d91858bc5e504ea996c64
mariovenditto-ctrl <mario.venditto@gmail.com>
2026-09-15 11:03:41 +0200

    Add src package init

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+Wall Damage Analyzer - IA per analizzare danni su pareti intonacate
+"""
+
+__version__ = "1.0.0"
+__author__ = "mariovenditto-ctrl"
+__description__ = "Analizza fotografie di pareti intonacate usando computer vision"
